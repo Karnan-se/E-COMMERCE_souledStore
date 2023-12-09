965b90265a88bfce5ec68630105965d58e23ac6e
karnan-se <karnan.se@gmail.com>
2023-12-09 10:48:15 +0530

    major changes

--- controller/admincontroller/admincontroller.js
+++ controller/admincontroller/admincontroller.js
@@ -1,8 +1,232 @@
 let adminLogin= async(req, res)=>{
-    res.render("admin/adminlogin.ejs",{message:null})
-}
-let admindashboard = async(req, res)=>{
-    res.render("admin/dashboard.ejs")
+    try {
+        res.render("admin/adminlogin.ejs",{message:null})
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
 
-module.exports={adminLogin, admindashboard}
+let admindashboard = async(req, res)=>{
+    try {
+        res.render("admin/dashboard.ejs")
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+let page_products_list = async(req, res)=>{
+    try {
+        res.render("admin/page-products-list.ejs")
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+let page_products_grid = async(req, res)=>{
+    try {
+        res.render("admin/page-products-grid.ejs")
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+let page_products_grid_2 = async(req, res)=>{
+    try {
+        res.render("admin/page-products-grid-2.ejs")
+    } catch (error) {
+        console.log(error)
+    }
+}
+let page_categories = async(req, res)=>{
+   try {
+    res.render("admin/page-categories.ejs")
+    
+   } catch (error) {
+    
+   }
+}
+let page_orders_1 = async(req, res)=>{
+    try {
+        res.render("admin/page-orders-1.ejs")
+        
+    } catch (error) {
+        
+    }
+    
+}
+
+let page_orders_2 = async(req, res)=>{
+    try {
+        res.render("admin/page-orders-2.ejs")
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_orders_detail = async(req, res)=>{
+    try {
+        res.render("admin/page-orders-details.ejs")
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+let page_sellers_cards = async(req, res)=>{
+    try {
+        res.render("admin/page-sellers-cards.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_sellers_list = async(req, res)=>{
+    try {
+        res.render("admin/page-sellers-list.ejs")
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+let page_seller_detail = async(req, res)=>{
+    try {
+        res.render("admin/page-seller-detail.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_form_product_1= async(req, res)=>{
+    try {
+        res.render("admin/page-form-product-1.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_form_product_2= async(req, res)=>{
+    try {
+        res.render("admin/page-form-product-2.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_form_product_3= async(req, res)=>{
+    try {
+        res.render("admin/page-form-product-3.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_form_product_4= async(req, res)=>{
+    try {
+        res.render("admin/page-form-product-3.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_transaction_1= async(req, res)=>{
+    try {
+        res.render("admin/page-transaction-1.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_transaction_2= async(req, res)=>{
+    try {
+        res.render("admin/page-transaction-2.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_account_login= async(req, res)=>{
+    try {
+        res.render("admin/page-account-login.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_account_register= async(req, res)=>{
+    try {
+        res.render("admin/page-account-register.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_reviews= async(req, res)=>{
+    try {
+        res.render("admin/page-reviews.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_brands= async(req, res)=>{
+    try {
+        res.render("admin/page-brands.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_settings_1= async(req, res)=>{
+    try {
+        res.render("admin/page-settings-1.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_settings_2= async(req, res)=>{
+    try {
+        res.render("admin/page-settings-2.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+let page_blank= async(req, res)=>{
+    try {
+        res.render("admin/page-blank.ejs")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+module.exports={adminLogin, 
+    admindashboard, 
+    page_products_list,
+    page_products_grid,
+    page_products_grid_2,
+    page_categories,
+    page_orders_1,
+    page_orders_2,
+    page_orders_detail,
+    page_sellers_cards,
+    page_sellers_list,
+    page_seller_detail,
+    page_form_product_1,
+    page_form_product_2,
+    page_form_product_3,
+    page_form_product_4,
+    page_transaction_1,
+    page_transaction_2,
+    page_account_login,
+    page_account_register,
+    page_reviews,
+    page_brands,
+    page_settings_1,
+    page_settings_2,
+    page_blank}
--- route/adminroute/route.js
+++ route/adminroute/route.js
@@ -4,7 +4,32 @@ const adminController= require("../../controller/admincontroller/admincontroller
 const admin = require("../../models/admin/admin");
 
 router.get("/admin",adminController.adminLogin);
-router.get("/register",adminController.admindashboard)
+router.get("/dashboard",adminController.admindashboard)
+router.get("/admin", adminController.adminLogin);
+router.get("/page-products-list", adminController.page_products_list);
+router.get("/page-products-grid", adminController.page_products_grid);
+router.get("/page-products-grid-2", adminController.page_products_grid_2);
+router.get("/page-categories", adminController.page_categories);
+router.get("/page-orders-1", adminController.page_orders_1);
+router.get("/page-orders-2", adminController.page_orders_2);
+router.get("/page-orders-detail", adminController.page_orders_detail);
+router.get("/page-sellers-cards", adminController.page_sellers_cards);
+router.get("/page-sellers-list", adminController.page_sellers_list);
+router.get("/page-seller-detail", adminController.page_seller_detail);
+router.get("/page-form-product-1", adminController.page_form_product_1);
+router.get("/page-form-product-2", adminController.page_form_product_2);
+router.get("/page-form-product-3", adminController.page_form_product_3);
+router.get("/page-form-product-4", adminController.page_form_product_4);
+router.get("/page-transaction-1", adminController.page_transaction_1);
+router.get("/page-transaction-2", adminController.page_transaction_2);
+router.get("/page-account-login", adminController.page_account_login);
+router.get("/page-account-register", adminController.page_account_register);
+router.get("/page-reviews", adminController.page_reviews);
+router.get("/page-brands", adminController.page_brands);
+router.get("/page-settings-1", adminController.page_settings_1);
+router.get("/page-settings-2", adminController.page_settings_2);
+router.get("/page-blank", adminController.page_blank);
+
 
 
 
